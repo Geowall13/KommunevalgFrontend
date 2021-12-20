c37628bda36d851ffaf3934c4f0f7acfaad934bf
Geowall13 <rasmushr13@hotmail.com>
2021-12-20 10:06:10 +0100

    Init

---
+++ src/main/java/com/exam2021/kommunevalgfrontend/KommunevalgFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.exam2021.kommunevalgfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KommunevalgFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KommunevalgFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exam2021/kommunevalgfrontend/KommunevalgFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam2021.kommunevalgfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KommunevalgFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
